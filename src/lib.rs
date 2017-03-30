4d74081d3be7ce79faf27797667f67c6f1506f28
Jan Walter <jdb.walter@posteo.de>
2017-03-30 11:18:25 +0200

    Implement sobol_2d (needs gray_code_sample_2d).

--- src/lib.rs
+++ src/lib.rs
@@ -4507,8 +4507,14 @@ impl ZeroTwoSequenceSampler {
         for samples in &mut self.samples_1d {
             van_der_corput(1,
                            self.samples_per_pixel as i32,
-                           samples, // self.samples_1d[i][0]
+                           samples,
                            &mut self.rng);
+        }
+        for samples in &mut self.samples_2d {
+            sobol_2d(1,
+                     self.samples_per_pixel as i32,
+                     samples,
+                     &mut self.rng);
         }
         // WORK
     }
@@ -4533,15 +4539,28 @@ impl ZeroTwoSequenceSampler {
 
 // see lowdiscrepancy.h
 
-/// Takes a generator matrix *c*, a number of samples to generate *n*,
-/// and stores the corresponding samples in memory at the location
-/// pointed to by *p*.
-pub fn gray_code_sample(c: [u32; 32], n: u32, scramble: u32, p: &mut [Float]) {
+/// Takes a generator matrix *c*, a number of 1D samples to generate
+/// *n*, and stores the corresponding samples in memory at the
+/// location pointed to by *p*.
+pub fn gray_code_sample_1d(c: [u32; 32], n: u32, scramble: u32, p: &mut [Float]) {
     let mut v: u32 = scramble;
     for i in 0..n as usize {
         // 1/2^32
         p[i] = (v as Float * 2.3283064365386963e-10 as Float).min(ONE_MINUS_EPSILON);
         v ^= c[(i + 1).trailing_zeros() as usize];
+    }
+}
+
+/// Takes two generator matrices *c0* and *c1*, a number of 2D samples
+/// to generate *n*, and stores the corresponding samples in memory at
+/// the location pointed to by *p*.
+pub fn gray_code_sample_2d(c0: &[u32], c1: &[u32], n: u32, scramble: &Point2i, p: &mut [Point2f]) {
+    let mut v: [u32; 2] = [scramble.x as u32, scramble.y as u32];
+    for i in 0..n as usize {
+        p[i].x = (v[0] as Float * 2.3283064365386963e-10 as Float).min(ONE_MINUS_EPSILON);
+        p[i].y = (v[1] as Float * 2.3283064365386963e-10 as Float).min(ONE_MINUS_EPSILON);
+        v[0] ^= c0[(i + 1).trailing_zeros() as usize];
+        v[1] ^= c1[(i + 1).trailing_zeros() as usize];
     }
 }
 
@@ -4559,8 +4578,71 @@ pub fn van_der_corput(n_samples_per_pixel_sample: i32,
                                        0x8000, 0x4000, 0x2000, 0x1000, 0x800, 0x400, 0x200, 0x100,
                                        0x80, 0x40, 0x20, 0x10, 0x8, 0x4, 0x2, 0x1];
     let total_samples: i32 = n_samples_per_pixel_sample * n_pixel_samples;
-    gray_code_sample(c_van_der_corput, total_samples as u32, scramble, samples);
+    gray_code_sample_1d(c_van_der_corput, total_samples as u32, scramble, samples);
     // randomly shuffle 1D sample points
+    for i in 0..n_pixel_samples as usize {
+        shuffle(&mut samples[(i * n_samples_per_pixel_sample as usize)..],
+                n_samples_per_pixel_sample,
+                1,
+                rng);
+    }
+    shuffle(&mut samples[..],
+            n_pixel_samples,
+            n_samples_per_pixel_sample,
+            rng);
+}
+
+pub fn sobol_2d(n_samples_per_pixel_sample: i32,
+                n_pixel_samples: i32,
+                samples: &mut [Point2f],
+                rng: &mut Rng) {
+    let x: i32 = rng.uniform_uint32() as i32;
+    let y: i32 = rng.uniform_uint32() as i32;
+    let scramble: Point2i = Point2i { x: x, y: y };
+    // define 2D Sobol$'$ generator matrices _c_sobol[2]_
+    let c_sobol: [[u32; 32]; 2] =
+        [[0x80000000_u32,
+          0x40000000,
+          0x20000000,
+          0x10000000,
+          0x8000000,
+          0x4000000,
+          0x2000000,
+          0x1000000,
+          0x800000,
+          0x400000,
+          0x200000,
+          0x100000,
+          0x80000,
+          0x40000,
+          0x20000,
+          0x10000,
+          0x8000,
+          0x4000,
+          0x2000,
+          0x1000,
+          0x800,
+          0x400,
+          0x200,
+          0x100,
+          0x80,
+          0x40,
+          0x20,
+          0x10,
+          0x8,
+          0x4,
+          0x2,
+          0x1],
+         [0x80000000, 0xc0000000, 0xa0000000, 0xf0000000, 0x88000000, 0xcc000000, 0xaa000000,
+          0xff000000, 0x80800000, 0xc0c00000, 0xa0a00000, 0xf0f00000, 0x88880000, 0xcccc0000,
+          0xaaaa0000, 0xffff0000, 0x80008000, 0xc000c000, 0xa000a000, 0xf000f000, 0x88008800,
+          0xcc00cc00, 0xaa00aa00, 0xff00ff00, 0x80808080, 0xc0c0c0c0, 0xa0a0a0a0, 0xf0f0f0f0,
+          0x88888888, 0xcccccccc, 0xaaaaaaaa, 0xffffffff]];
+    gray_code_sample_2d(&c_sobol[0],
+                        &c_sobol[1],
+                        (n_samples_per_pixel_sample * n_pixel_samples) as u32,
+                        &scramble,
+                        &mut samples[..]);
     for i in 0..n_pixel_samples as usize {
         shuffle(&mut samples[(i * n_samples_per_pixel_sample as usize)..],
                 n_samples_per_pixel_sample,
