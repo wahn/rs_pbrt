5a3593a28dd9bcb0f7d330ae84dee3bf18ca8a81
Jan Walter <jan@janwalter.com>
2017-06-15 11:41:51 +0100

    Prepare for some command line options:
    > ./target/release/examples/pest_test -h
    Usage: ./target/release/examples/pest_test [options]
    
    Options:
        -h, --help          print this help menu
        -v, --version       print version number
    
    > ./target/release/examples/pest_test -v
    ./target/release/examples/pest_test 0.1.7

--- examples/pest_test.rs
+++ examples/pest_test.rs
@@ -1,8 +1,16 @@
 #[macro_use]
 extern crate pest;
+extern crate getopts;
 
+// parser
 use pest::prelude::*;
+// getopts
+use getopts::Options;
+// std
 use std::collections::LinkedList;
+use std::env;
+
+pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
     
 #[derive(Debug, PartialEq)]
 pub enum Node {
@@ -49,7 +57,34 @@ impl_rdp! {
     }
 }
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn print_version(program: &str) {
+    println!("{} {}", program, VERSION);
+}
+
 fn main() {
+    // handle command line options
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("v", "version", "print version number");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    } else if matches.opt_present("v") {
+        print_version(&program);
+        return;
+    }
+    // parser
     let mut parser = Rdp::new(StringInput::new("abc def"));
 
     assert!(parser.sentence());
