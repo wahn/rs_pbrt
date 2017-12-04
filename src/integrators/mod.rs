bb9b87b16f6b8b97d86581ebf00fdcccdc055d57
Jan Walter <jdb.walter@posteo.de>
2017-12-04 19:26:16 +0100

    Add pictures and explain the current three integrators.

--- src/integrators/mod.rs
+++ src/integrators/mod.rs
@@ -1,6 +1,14 @@
 //! # Ambient Occlusion (AO)
 //!
-//! TODO
+//! Ambient Occlusion is most often calculated by casting rays in
+//! every direction from the surface. Rays which reach the background
+//! or sky increase the brightness of the surface, whereas a ray which
+//! hits any other object contributes no illumination. As a result,
+//! points surrounded by a large amount of geometry are rendered dark,
+//! whereas points with little geometry on the visible hemisphere
+//! appear light.
+//!
+//! ![Ambient Occlusion](https://www.janwalter.org/assets/cornell_box_pbrt_rust_ao.png)
 //!
 //! # Direct Lighting
 //!
@@ -10,9 +18,14 @@
 //! objects that are not themselfes emissive, except for basic
 //! specular reflection and transmission effects.
 //!
+//! ![Direct Lighting](https://www.janwalter.org/assets/cornell_box_v0.1.13.png)
+//!
 //! # Path Tracing
 //!
-//! TODO
+//! Path tracing incrementally generates paths of scattering events
+//! starting at the camera and ending at light sources in the scene.
+//!
+//! ![Path Tracing](https://www.janwalter.org/assets/cornell_box_v0.2.0_high.png)
 
 pub mod ao;
 pub mod directlighting;
