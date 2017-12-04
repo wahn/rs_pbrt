5e7e6e93040494eee7506dc955d83534e01aa8bb
Jan Walter <jdb.walter@posteo.de>
2017-12-04 20:12:50 +0100

    Add an image from Wikipedia for a Perspective Camera.

--- src/cameras/mod.rs
+++ src/cameras/mod.rs
@@ -1,1 +1,11 @@
+//! Perspective Camera
+//!
+//! The two most characteristic features of perspective are that
+//! objects are smaller as their distance from the observer increases;
+//! and that they are subject to **foreshortening**, meaning that an
+//! object's dimensions along the line of sight are shorter than its
+//! dimensions across the line of sight.
+//!
+//! ![Perspective Camera](https://www.janwalter.org/assets/Perspectiva-2.png)
+
 pub mod perspective;
