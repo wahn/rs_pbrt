b9d4789def94f1a2051f53d544e74dc63ceff278
Jan Walter <jdb.walter@posteo.de>
2018-02-13 11:34:43 +0100

    We need EndpointInteraction and Vertex.

--- src/integrators/bdpt.rs
+++ src/integrators/bdpt.rs
@@ -1,10 +1,59 @@
+// std
+use std;
 // pbrt
 use core::camera::{Camera, CameraSample};
-use core::geometry::{Bounds2i, Point2f, Ray};
+use core::geometry::{Bounds2i, Normal3f, Point2f, Point3f, Ray, Vector3f};
+use core::geometry::pnt3_offset_ray_origin;
+use core::interaction::Interaction;
 use core::pbrt::{Float, Spectrum};
 use core::sampler::Sampler;
 
 // see bdpt.h
+
+pub struct EndpointInteraction {
+    // Interaction Public Data
+    pub p: Point3f,
+    pub time: Float,
+    pub p_error: Vector3f,
+    pub wo: Vector3f,
+    pub n: Normal3f,
+}
+
+impl EndpointInteraction {}
+
+impl Interaction for EndpointInteraction {
+    fn is_surface_interaction(&self) -> bool {
+        self.n != Normal3f::default()
+    }
+    fn is_medium_interaction(&self) -> bool {
+        !self.is_surface_interaction()
+    }
+    fn spawn_ray(&self, d: &Vector3f) -> Ray {
+        let o: Point3f = pnt3_offset_ray_origin(&self.p, &self.p_error, &self.n, d);
+        Ray {
+            o: o,
+            d: *d,
+            t_max: std::f32::INFINITY,
+            time: self.time,
+            differential: None,
+        }
+    }
+    fn get_p(&self) -> Point3f {
+        self.p.clone()
+    }
+    fn get_time(&self) -> Float {
+        self.time
+    }
+    fn get_p_error(&self) -> Vector3f {
+        self.p_error.clone()
+    }
+    fn get_wo(&self) -> Vector3f {
+        self.wo.clone()
+    }
+    fn get_n(&self) -> Normal3f {
+        self.n.clone()
+    }
+}
 
 pub enum VertexType {
     Camera,
@@ -19,6 +68,13 @@ pub struct Vertex {
     delta: bool,
     pdf_fwd: Float,
     pdf_rev: Float,
+}
+
+impl Vertex {
+    // pub fn new() -> Vertex {}
+    // pub fn create_camera(camera: &Box<Camera + Send + Sync>, ray: &Ray, beta: &Spectrum) -> Vertex {
+    //     Vertex::new(VertexType::Camera, EndpointInteraction(camera, ray), beta)
+    // }
 }
 
 /// Bidirectional Path Tracing (Global Illumination)
