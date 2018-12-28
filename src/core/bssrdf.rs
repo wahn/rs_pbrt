283d0a9d9a6bd369b4517d008a6d5f7a5f16c0be
Jan Walter <jan@janwalter.com>
2018-12-28 10:03:14 +0000

    Where are the Vec<Float> of BSSRDFTable filled with values?

--- src/core/bssrdf.rs
+++ src/core/bssrdf.rs
@@ -1,2 +1,31 @@
+//std
+use std;
+// pbrt
+use core::pbrt::{Float, Spectrum};
+
 pub struct BSSRDFTable {
+    pub n_rho_samples: i32,
+    pub n_radius_samples: i32,
+    pub rho_samples: Vec<Float>,
+    pub radius_samples: Vec<Float>,
+    pub profile: Vec<Float>,
+    pub rho_eff: Vec<Float>,
+    pub profile_cdf: Vec<Float>,
+}
+
+impl BSSRDFTable {
+    pub fn new(n_rho_samples: i32, n_radius_samples: i32) -> Self {
+        BSSRDFTable {
+            n_rho_samples: n_rho_samples,
+            n_radius_samples: n_radius_samples,
+            rho_samples: Vec::with_capacity(n_rho_samples as usize),
+            radius_samples: Vec::with_capacity(n_radius_samples as usize),
+            profile: Vec::with_capacity((n_radius_samples * n_rho_samples) as usize),
+            rho_eff: Vec::with_capacity(n_rho_samples as usize),
+            profile_cdf: Vec::with_capacity((n_radius_samples * n_rho_samples) as usize),
+        }
+    }
+    pub fn eval_profile(&self, rho_index: i32, radius_index: i32) -> Float {
+        self.profile[(rho_index * self.n_radius_samples + radius_index) as usize]
+    }
 }
