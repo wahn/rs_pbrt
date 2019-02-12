2bf63423846c5244ce53badca34ba15427b4b4b6
Jan Walter <jdb.walter@posteo.de>
2019-02-12 17:38:18 +0100

    Let's use Vec<SPPMPixel> instead of Vec<Arc<SPPMPixel>>.

--- src/integrators/sppm.rs
+++ src/integrators/sppm.rs
@@ -138,11 +138,11 @@ pub fn render_sppm(
     let film: Arc<Film> = camera.get_film();
     let pixel_bounds: Bounds2i = film.cropped_pixel_bounds;
     let n_pixels: i32 = pixel_bounds.area();
-    let mut pixels: Vec<Arc<SPPMPixel>> = Vec::with_capacity(n_pixels as usize);
+    let mut pixels: Vec<SPPMPixel> = Vec::with_capacity(n_pixels as usize);
     for _i in 0..n_pixels as usize {
         let mut pixel = SPPMPixel::default();
         pixel.radius = integrator.initial_search_radius;
-        pixels.push(Arc::new(pixel));
+        pixels.push(pixel);
     }
     let inv_sqrt_spp: Float = 1.0 as Float / (integrator.n_iterations as Float).sqrt();
     // TODO: let pixel_memory_bytes: usize = n_pixels as usize * std::mem::size_of::<SPPMPixel>();
@@ -206,7 +206,7 @@ pub fn render_sppm(
                             let p_pixel_o: Point2i = Point2i::from(p_pixel - pixel_bounds.p_min);
                             let pixel_offset: i32 = p_pixel_o.x
                                 + p_pixel_o.y * (pixel_bounds.p_max.x - pixel_bounds.p_min.x);
-                            if let Some(pixel) = Arc::get_mut(&mut pixels[pixel_offset as usize]) {
+                            let mut pixel = &mut pixels[pixel_offset as usize];
                                 let mut specular_bounce: bool = false;
                                 for depth in 0..integrator.max_depth {
                                     // TODO: ++totalPhotonSurfaceInteractions;
@@ -300,7 +300,6 @@ pub fn render_sppm(
                                         break;
                                     }
                                 }
-                            }
                         }
                     }
                 }
@@ -321,7 +320,7 @@ pub fn render_sppm(
                 let mut max_radius: Float = 0.0 as Float;
                 println!("Compute grid bounds for SPPM visible points ...");
                 for i in 0..n_pixels as usize {
-                    if let Some(pixel) = Arc::get_mut(&mut pixels[i]) {
+                    let mut pixel = &mut pixels[i];
                         if pixel.vp.beta.is_black() {
                             continue;
                         }
@@ -334,7 +333,6 @@ pub fn render_sppm(
                         );
                         grid_bounds = bnd3_union_bnd3(&grid_bounds, &vp_bound);
                         max_radius = max_radius.max(pixel.radius);
-                    }
                 }
                 // compute resolution of SPPM grid in each dimension
                 let diag: Vector3f = grid_bounds.diagonal();
@@ -349,7 +347,7 @@ pub fn render_sppm(
                 // TODO: ParallelFor([&](int pixelIndex) { ... }, nPixels, 4096);
                 println!("Add visible points to SPPM grid ...");
                 for pixel_index in 0..n_pixels as usize {
-                    let pixel: &Arc<SPPMPixel> = &pixels[pixel_index];
+                    let mut pixel = &mut pixels[pixel_index];
                     if !pixel.vp.beta.is_black() {
                         // add pixel's visible point to applicable grid cells
                         let radius: Float = pixel.radius;
@@ -377,26 +375,26 @@ pub fn render_sppm(
                             &grid_res,
                             &mut p_max,
                         );
-                        for z in p_min.z..(p_max.z + 1) {
-                            for y in p_min.y..(p_max.y + 1) {
-                                for x in p_min.x..(p_max.x + 1) {
-                                    // add visible point to grid cell $(x, y, z)$
-                                    let h: usize =
-                                        hash(&Point3i { x: x, y: y, z: z }, hash_size as i32);
-                                    let mut node_arc: Arc<SPPMPixelListNode> =
-                                        Arc::new(SPPMPixelListNode::default());
-                                    let pixel_clone: Arc<SPPMPixel> = pixel.clone();
-                                    if let Some(node) = Arc::get_mut(&mut node_arc) {
-                                        node.pixel = pixel_clone;
-                                        if !grid[h].is_none() {
-                                            node.next.set_if_none(grid[h].take().unwrap());
-                                        }
-                                    }
-                                    // atomically add _node_ to the start of _grid[h]_'s linked list
-                                    grid[h].set_if_none(node_arc);
-                                }
-                            }
-                        }
+                        // for z in p_min.z..(p_max.z + 1) {
+                        //     for y in p_min.y..(p_max.y + 1) {
+                        //         for x in p_min.x..(p_max.x + 1) {
+                        //             // add visible point to grid cell $(x, y, z)$
+                        //             let h: usize =
+                        //                 hash(&Point3i { x: x, y: y, z: z }, hash_size as i32);
+                        //             let mut node_arc: Arc<SPPMPixelListNode> =
+                        //                 Arc::new(SPPMPixelListNode::default());
+                        //             let pixel_clone: Arc<SPPMPixel> = pixel.clone();
+                        //             if let Some(node) = Arc::get_mut(&mut node_arc) {
+                        //                 node.pixel = pixel_clone;
+                        //                 if !grid[h].is_none() {
+                        //                     node.next.set_if_none(grid[h].take().unwrap());
+                        //                 }
+                        //             }
+                        //             // atomically add _node_ to the start of _grid[h]_'s linked list
+                        //             grid[h].set_if_none(node_arc);
+                        //         }
+                        //     }
+                        // }
                         // ReportValue(grid_cells_per_visible_point,
                         //             (1 + pMax.x - pMin.x) * (1 + pMax.y - pMin.y) *
                         //                 (1 + pMax.z - pMin.z));
@@ -597,7 +595,7 @@ pub fn render_sppm(
                     let mut p_phi: [Float; 3] = [0.0 as Float; 3];
                     let p_vp_beta: Spectrum;
                     {
-                        let p: &Arc<SPPMPixel> = &pixels[i];
+                        let p = &pixels[i];
                         p_n = p.n;
                         p_tau = p.tau;
                         p_radius = p.radius;
@@ -607,9 +605,8 @@ pub fn render_sppm(
                         p_vp_beta = p.vp.beta;
                     }
                     // ... before borrowing mutably
-                    let p: &mut Arc<SPPMPixel> = &mut pixels[i];
+                    let mut p = &mut pixels[i];
                     let p_m = p.m.load(atomic::Ordering::Relaxed);
-                    if let Some(p_mut) = Arc::<SPPMPixel>::get_mut(p) {
                         if p_m > 0_i32 {
                             // update pixel photon count, search radius, and $\tau$ from photons
                             let gamma: Float = 2.0 as Float / 3.0 as Float;
@@ -619,19 +616,18 @@ pub fn render_sppm(
                             for j in 0..3 {
                                 phi[j] = p_phi[j];
                             }
-                            p_mut.tau =
+                            p.tau =
                                 (p_tau + p_vp_beta * phi) * (r_new * r_new) / (p_radius * p_radius);
-                            p_mut.n = n_new;
-                            p_mut.radius = r_new;
-                            p_mut.m.store(0, atomic::Ordering::Relaxed);
+                            p.n = n_new;
+                            p.radius = r_new;
+                            p.m.store(0, atomic::Ordering::Relaxed);
                             for j in 0..3 {
-                                p_mut.phi[j] = AtomicFloat::new(0.0 as Float);
+                                p.phi[j] = AtomicFloat::new(0.0 as Float);
                             }
                         }
                         // reset _VisiblePoint_ in pixel
-                        p_mut.vp.beta = Spectrum::default();
-                        p_mut.vp.bsdf = None;
-                    }
+                        p.vp.beta = Spectrum::default();
+                        p.vp.bsdf = None;
                 }
             }
             // periodically store SPPM image in film and write image
@@ -644,7 +640,7 @@ pub fn render_sppm(
                 for y in (pixel_bounds.p_min.y as usize)..(pixel_bounds.p_max.y as usize) {
                     for x in (x0 as usize)..(x1 as usize) {
                         // compute radiance _L_ for SPPM pixel _pixel_
-                        let pixel: &Arc<SPPMPixel> = &pixels[(y - pixel_bounds.p_min.y as usize)
+                        let pixel = &pixels[(y - pixel_bounds.p_min.y as usize)
                             * (x1 as usize - x0 as usize)
                             + (x - x0 as usize)];
                         let mut l: Spectrum = pixel.ld / (iter + 1) as Float;
