27416fbed92eaef7f935c809bb8a32981748beac
Jan Walter <jdb.walter@posteo.de>
2019-04-01 18:43:49 +0200

    Re-write parser?

---
+++ examples/parse_pbrt_file.rs
@@ -0,0 +1,41 @@
+// AttributeBegin
+// AttributeEnd
+// ActiveTransform
+// AreaLightSource
+// Accelerator
+// ConcatTransform
+// CoordinateSystem
+// CoordSysTransform
+// Camera
+// Film
+// Integrator
+// Include
+// Identity
+// LightSource
+// LookAt
+// MakeNamedMaterial
+// MakeNamedMedium
+// Material
+// MediumInterface
+// NamedMaterial
+// ObjectBegin
+// ObjectEnd
+// ObjectInstance
+// PixelFilter
+// ReverseOrientation
+// Rotate
+// Shape
+// Sampler
+// Scale
+// TransformBegin
+// TransformEnd
+// Transform
+// Translate
+// TransformTimes
+// Texture
+// WorldBegin
+// WorldEnd
+
+fn main() {
+    println!("Hello, world!");
+}
