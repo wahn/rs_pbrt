2345c0b02cf196934cf3bdeba428c4671a50243c
Jan Walter <jdb.walter@posteo.de>
2019-05-01 15:52:18 +0200

    Start to implement VolPathIntegrator::li(...).

--- src/integrators/volpath.rs
+++ src/integrators/volpath.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 // pbrt
 use core::geometry::{Bounds2i, Point2f, Ray, Vector3f};
 use core::integrator::SamplerIntegrator;
+use core::interaction::{Interaction, InteractionCommon, MediumInteraction, SurfaceInteraction};
 use core::lightdistrib::create_light_sample_distribution;
 use core::lightdistrib::LightDistribution;
 use core::pbrt::{Float, Spectrum};
@@ -53,6 +54,49 @@ impl SamplerIntegrator for VolPathIntegrator {
         // arena: &mut Arena,
         _depth: i32,
     ) -> Spectrum {
+        // TODO: ProfilePhase p(Prof::SamplerIntegratorLi);
+        let mut l: Spectrum = Spectrum::default();
+        let mut beta: Spectrum = Spectrum::new(1.0 as Float);
+        let mut ray: Ray = Ray {
+            o: r.o,
+            d: r.d,
+            t_max: r.t_max,
+            time: r.time,
+            differential: r.differential,
+            medium: r.medium.clone(),
+        };
+        let mut specular_bounce: bool = false;
+        let mut bounces: u32 = 0_u32;
+        // Added after book publication: etaScale tracks the
+        // accumulated effect of radiance scaling due to rays passing
+        // through refractive boundaries (see the derivation on p. 527
+        // of the third edition). We track this value in order to
+        // remove it from beta when we apply Russian roulette; this is
+        // worthwhile, since it lets us sometimes avoid terminating
+        // refracted rays that are about to be refracted back out of a
+        // medium and thus have their beta value increased.
+        let mut eta_scale: Float = 1.0;
+        loop {
+            // intersect _ray_ with scene and store intersection in _isect_
+            let found_intersection: bool;
+            if let Some(mut isect) = scene.intersect(&mut ray) {
+                found_intersection = true;
+                // sample the participating medium, if present
+                let mut mi_opt: Option<MediumInteraction> = None;
+                if let Some(ref medium) = ray.medium {
+                    let (spectrum, option) = medium.sample(&ray, sampler.as_mut());
+                    beta *= spectrum;
+                    if let Some(mi) = option {
+                        mi_opt = Some(mi);
+                    }
+                }
+                if beta.is_black() {
+                    break;
+                }
+            } else {
+                found_intersection = false;
+            }
+        }
         // WORK
         Spectrum::default()
     }
