4b62868d11e3e7e1d64d444cdeac2a6c31cbb7b2
Jan Walter <jan@janwalter.com>
2019-05-15 08:06:12 +0100

    Add rendered image of a smoke plume for GridDensityMedium.

--- src/media/mod.rs
+++ src/media/mod.rs
@@ -5,6 +5,10 @@
 //! - GridDensityMedium
 //! - HomogeneousMedium
 //!
+//! ## Grid Density Medium
+//!
+//! ![Smoke from a CFD Simulation](/doc/img/smoke_plume_pbrt_rust_volpath.png)
+//!
 //! ## Homogeneous Medium
 //!
 //! ![A Volumetric Caustic](/doc/img/volume_caustic_pbrt_rust_mlt.png)
