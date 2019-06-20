820a6cdbdb170f944f664512d615a43eb25c3f53
Daniel Egger <daniel@eggers-club.de>
2019-06-20 18:18:41 +0200

    Cleanup shape struct initialisation
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/shapes/disk.rs
+++ src/shapes/disk.rs
@@ -33,7 +33,7 @@ impl Default for Disk {
         let object_to_world: Transform = Transform::default();
         Disk {
             // Shape
-            object_to_world: object_to_world,
+            object_to_world,
             world_to_object: Transform::default(),
             reverse_orientation: false,
             transform_swaps_handedness: object_to_world.swaps_handedness(),
@@ -59,14 +59,14 @@ impl Disk {
     ) -> Self {
         Disk {
             // Shape
-            object_to_world: object_to_world,
-            world_to_object: world_to_object,
-            reverse_orientation: reverse_orientation,
+            object_to_world,
+            world_to_object,
+            reverse_orientation,
             transform_swaps_handedness: object_to_world.swaps_handedness(),
             // Disk
-            height: height,
-            radius: radius,
-            inner_radius: inner_radius,
+            height,
+            radius,
+            inner_radius,
             phi_max: radians(clamp_t(phi_max, 0.0, 360.0)),
             material: None,
         }
