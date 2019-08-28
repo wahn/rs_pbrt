085100acd6ee979b019049a9eabbf8865ad02589
Jan Walter <jan@janwalter.com>
2019-08-28 18:50:34 +0100

    Implement DotsTexture<T>::evaluate(...).

--- src/textures/dots.rs
+++ src/textures/dots.rs
@@ -1,22 +1,23 @@
 // std
 use std::sync::Arc;
-// others
-use num;
 // pbrt
+use crate::core::geometry::{Point2f, Vector2f};
 use crate::core::interaction::SurfaceInteraction;
-use crate::core::texture::{Texture, TextureMapping3D};
+use crate::core::pbrt::Float;
+use crate::core::texture::noise_flt;
+use crate::core::texture::{Texture, TextureMapping2D};
 
 // see dots.h
 
 pub struct DotsTexture<T> {
-    pub mapping: Box<dyn TextureMapping3D + Send + Sync>,
+    pub mapping: Box<dyn TextureMapping2D + Send + Sync>,
     pub outside_dot: Arc<dyn Texture<T> + Send + Sync>,
     pub inside_dot: Arc<dyn Texture<T> + Send + Sync>,
 }
 
 impl<T: Copy> DotsTexture<T> {
     pub fn new(
-        mapping: Box<dyn TextureMapping3D + Send + Sync>,
+        mapping: Box<dyn TextureMapping2D + Send + Sync>,
         outside_dot: Arc<dyn Texture<T> + Send + Sync>,
         inside_dot: Arc<dyn Texture<T> + Send + Sync>,
     ) -> Self {
@@ -28,12 +29,47 @@ impl<T: Copy> DotsTexture<T> {
     }
 }
 
-impl<T: Copy> Texture<T> for DotsTexture<T>
-where
-    T: num::Zero,
-{
-    fn evaluate(&self, _si: &SurfaceInteraction) -> T {
-        // WORK
-        T::from(num::Zero::zero())
+impl<T: Copy> Texture<T> for DotsTexture<T> {
+    fn evaluate(&self, si: &SurfaceInteraction) -> T {
+        // compute cell indices for dots
+        let mut dpdx: Vector2f = Vector2f::default();
+        let mut dpdy: Vector2f = Vector2f::default();
+        let st: Point2f = self.mapping.map(si, &mut dpdx, &mut dpdy);
+        // int s_cell = std::floor(st[0] + .5f), t_cell = std::floor(st[1] + .5f);
+        let s_cell: i32 = (st.x + 0.5 as Float).floor() as i32;
+        let t_cell: i32 = (st.y + 0.5 as Float).floor() as i32;
+        // return _insideDot_ result if point is inside dot
+        if noise_flt(
+            s_cell as Float + 0.5 as Float,
+            t_cell as Float + 0.5 as Float,
+            0.5 as Float, // default
+        ) > 0.5 as Float
+        {
+            let radius: Float = 0.35 as Float;
+            let max_shift: Float = 0.5 as Float - radius;
+            let s_center: Float = s_cell as Float
+                + max_shift
+                    * noise_flt(
+                        s_cell as Float + 1.5 as Float,
+                        t_cell as Float + 2.8 as Float,
+                        0.5 as Float, // default
+                    );
+            let t_center: Float = t_cell as Float
+                + max_shift
+                    * noise_flt(
+                        s_cell as Float + 4.5 as Float,
+                        t_cell as Float + 9.8 as Float,
+                        0.5 as Float, // default
+                    );
+            let dst: Vector2f = st
+                - Point2f {
+                    x: s_center,
+                    y: t_center,
+                };
+            if dst.length_squared() < radius * radius {
+                return self.inside_dot.evaluate(si);
+            }
+        }
+        self.outside_dot.evaluate(si)
     }
 }
