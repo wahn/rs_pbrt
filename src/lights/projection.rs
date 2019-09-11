8e830a6519127d5c67a627beb52ea19f3e781458
Jan Walter <jan@janwalter.com>
2019-09-11 13:30:24 +0100

    Use LightFlags::DeltaPosition and add constructor for openexr.

--- src/lights/projection.rs
+++ src/lights/projection.rs
@@ -47,6 +47,152 @@ impl ProjectionLight {
     ) -> Self {
         ProjectionLight::new_hdr(light_to_world, medium_interface, i, texname, fov)
     }
+    #[cfg(feature = "openexr")]
+    pub fn new(
+        light_to_world: &Transform,
+        medium_interface: &MediumInterface,
+        i: &Spectrum,
+        texname: String,
+        fov: Float,
+    ) -> Self {
+        if texname != String::from("") {
+            // https://cessen.github.io/openexr-rs/openexr/index.html
+            let mut resolution: Point2i = Point2i::default();
+            let mut names_and_fills: Vec<(&str, f64)> = Vec::new();
+            // header
+            let file_result = std::fs::File::open(texname.clone());
+            if file_result.is_ok() {
+                let mut file = file_result.unwrap();
+                let input_file_result = InputFile::new(&mut file);
+                if input_file_result.is_ok() {
+                    let input_file = input_file_result.unwrap();
+                    // get resolution
+                    let (width, height) = input_file.header().data_dimensions();
+                    resolution.x = width as i32;
+                    resolution.y = height as i32;
+                    // make sure the image properties are the same (see incremental_io.rs in github/openexr-rs)
+                    for channel_name in ["R", "G", "B"].iter() {
+                        let channel = input_file
+                            .header()
+                            .get_channel(channel_name)
+                            .expect(&format!("Didn't find channel {}.", channel_name));
+                        assert!(channel.pixel_type == PixelType::HALF);
+                        names_and_fills.push((channel_name, 0.0_f64));
+                    }
+                    let mut pixel_data =
+                        vec![
+                            (f16::from_f32(0.0), f16::from_f32(0.0), f16::from_f32(0.0));
+                            (resolution.x * resolution.y) as usize
+                        ];
+                    {
+                        // read pixels
+                        let mut file = std::fs::File::open(texname.clone()).unwrap();
+                        let mut input_file = InputFile::new(&mut file).unwrap();
+                        let mut fb = FrameBufferMut::new(resolution.x as u32, resolution.y as u32);
+                        fb.insert_channels(&names_and_fills[..], &mut pixel_data);
+                        input_file.read_pixels(&mut fb).unwrap();
+                    }
+                    // convert pixel data into Vec<Spectrum> (and on the way multiply by _l_)
+                    let mut texels: Vec<Spectrum> = Vec::new();
+                    for i in 0..(resolution.x * resolution.y) {
+                        let (r, g, b) = pixel_data[i as usize];
+                        texels.push(
+                            Spectrum::rgb(
+                                decode_f16(r.to_bits()),
+                                decode_f16(g.to_bits()),
+                                decode_f16(b.to_bits()),
+                            ) * *l,
+                        );
+                    }
+                    // create _MipMap_ from converted texels (see above)
+                    let do_trilinear: bool = false;
+                    let max_aniso: Float = 8.0 as Float;
+                    let wrap_mode: ImageWrap = ImageWrap::Repeat;
+                    let projection_map = Arc::new(MipMap::new(
+                        &resolution,
+                        &texels[..],
+                        do_trilinear,
+                        max_aniso,
+                        wrap_mode,
+                    ));
+                    let p_light: Point3f = light_to_world.transform_point(&Point3f::default());
+                    let aspect: Float = resolution.x as Float / resolution.y as Float;
+                    let screen_bounds: Bounds2f;
+                    if aspect > 1.0 as Float {
+                        screen_bounds = Bounds2f {
+                            p_min: Point2f {
+                                x: -aspect,
+                                y: -1.0 as Float,
+                            },
+                            p_max: Point2f {
+                                x: aspect,
+                                y: 1.0 as Float,
+                            },
+                        };
+                    } else {
+                        screen_bounds = Bounds2f {
+                            p_min: Point2f {
+                                x: -1.0 as Float,
+                                y: -1.0 as Float / aspect,
+                            },
+                            p_max: Point2f {
+                                x: 1.0 as Float,
+                                y: 1.0 as Float / aspect,
+                            },
+                        };
+                    }
+                    let hither: Float = 1e-3 as Float;
+                    let yon: Float = 1e30 as Float;
+                    let light_projection: Transform = Transform::perspective(fov, hither, yon);
+                    let screen_to_light: Transform = Transform::inverse(&light_projection);
+                    let p_corner: Point3f = Point3f {
+                        x: screen_bounds.p_max.x,
+                        y: screen_bounds.p_max.y,
+                        z: 0.0 as Float,
+                    };
+                    let w_corner: Vector3f =
+                        Vector3f::from(screen_to_light.transform_point(&p_corner)).normalize();
+                    let cos_total_width: Float = w_corner.z;
+                    return ProjectionLight {
+                        projection_map: Some(projection_map),
+                        p_light: p_light,
+                        i: *i,
+                        light_projection: light_projection,
+                        hither: hither,
+                        yon: yon,
+                        screen_bounds: screen_bounds,
+                        cos_total_width: cos_total_width,
+                        flags: LightFlags::DeltaPosition as u8,
+                        n_samples: 1_i32,
+                        medium_interface: MediumInterface::default(),
+                        light_to_world: *light_to_world,
+                        world_to_light: Transform::inverse(&*light_to_world),
+                    };
+                } else {
+                    // try to open an HDR image instead (TODO: check extension upfront)
+                    ProjectionLight::new_hdr(light_to_world, l, n_samples, texname)
+                }
+            } else {
+                // try to open an HDR image instead (TODO: check extension upfront)
+                ProjectionLight::new_hdr(light_to_world, l, n_samples, texname)
+            }
+        }
+        ProjectionLight {
+            projection_map: None,
+            p_light: Point3f::default(),
+            i: Spectrum::default(),
+            light_projection: Transform::default(),
+            hither: 0.0 as Float,
+            yon: 0.0 as Float,
+            screen_bounds: Bounds2f::default(),
+            cos_total_width: 0.0 as Float,
+            flags: LightFlags::DeltaPosition as u8,
+            n_samples: 1_i32,
+            medium_interface: MediumInterface::default(),
+            light_to_world: Transform::default(),
+            world_to_light: Transform::default(),
+        }
+    }
     pub fn new_hdr(
         light_to_world: &Transform,
         _medium_interface: &MediumInterface,
@@ -129,7 +275,7 @@ impl ProjectionLight {
                             yon: yon,
                             screen_bounds: screen_bounds,
                             cos_total_width: cos_total_width,
-                            flags: LightFlags::Infinite as u8,
+                            flags: LightFlags::DeltaPosition as u8,
                             n_samples: 1_i32,
                             medium_interface: MediumInterface::default(),
                             light_to_world: *light_to_world,
@@ -150,7 +296,7 @@ impl ProjectionLight {
             yon: 0.0 as Float,
             screen_bounds: Bounds2f::default(),
             cos_total_width: 0.0 as Float,
-            flags: LightFlags::Infinite as u8,
+            flags: LightFlags::DeltaPosition as u8,
             n_samples: 1_i32,
             medium_interface: MediumInterface::default(),
             light_to_world: Transform::default(),
