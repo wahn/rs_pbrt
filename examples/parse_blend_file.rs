012127ddff25963cfabd7e839338f9f7382091c1
Jan Walter <jan@janwalter.com>
2019-09-25 10:47:11 +0100

    Fix field of view (fov) calculation to match Python counterpart.

--- examples/parse_blend_file.rs
+++ examples/parse_blend_file.rs
@@ -2874,10 +2874,10 @@ fn main() -> std::io::Result<()> {
     }
     if let Some(cam) = camera_hm.get(&base_name) {
         // overwrite fov
-        if aspect > 1.0 {
+        if aspect >= 1.0 {
             fov = 2.0 as Float * degrees((16.0 as Float / (aspect * cam.lens)).atan());
         } else {
-            fov = 2.0 as Float * degrees((aspect * 16.0 as Float) / cam.lens).atan();
+            fov = 2.0 as Float * degrees(((aspect * 16.0 as Float) / cam.lens).atan());
         }
         // println!("fov[{}] overwritten", fov);
     }
