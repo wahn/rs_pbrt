193ff98a226b11b0f7799bd3ee3cd41a1739ac26
Jan Walter <jan@janwalter.com>
2019-10-11 13:38:44 +0100

    For SurfaceInteraction use lifetime instead of Arc to point to Primitive.

--- src/core/interaction.rs
+++ src/core/interaction.rs
@@ -277,7 +277,7 @@ pub struct SurfaceInteraction<'a> {
     pub dvdx: RwLock<Float>,
     pub dudy: RwLock<Float>,
     pub dvdy: RwLock<Float>,
-    pub primitive: Option<Arc<dyn Primitive + Send + Sync>>,
+    pub primitive: Option<&'a (dyn Primitive + Send + Sync)>,
     pub shading: Shading,
     pub bsdf: Option<Arc<Bsdf>>,
     pub bssrdf: Option<Arc<TabulatedBssrdf>>,
