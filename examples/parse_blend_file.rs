e970c0656d6204c896822e15554a717bfeac2881
Jan Walter <jan@janwalter.com>
2019-12-27 13:27:00 +0000

    Match the field of view (fov) between Blender and rs_pbrt.

--- examples/parse_blend_file.rs
+++ examples/parse_blend_file.rs
@@ -4286,7 +4286,7 @@ fn main() -> std::io::Result<()> {
     if let Some(cam) = camera_hm.get(&base_name) {
         // overwrite fov
         if aspect >= 1.0 {
-            fov = 2.0 as Float * degrees((16.0 as Float / (aspect * cam.lens)).atan());
+            fov = cam.angle_x / 2.0;
         } else {
             fov = 2.0 as Float * degrees(((aspect * 16.0 as Float) / cam.lens).atan());
         }
