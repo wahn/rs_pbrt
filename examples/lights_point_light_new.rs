986777c049ef7b0218e66a5563d61e487a8ce826
Jan Walter <jdb.walter@posteo.de>
2020-08-09 18:18:00 +0200

    Bring back example code.

---
+++ examples/lights_point_light_new.rs
@@ -0,0 +1,11 @@
+use pbrt::core::medium::MediumInterface;
+use pbrt::core::pbrt::Spectrum;
+use pbrt::core::transform::Transform;
+use pbrt::lights::point::PointLight;
+
+fn main() {
+    let i: Spectrum = Spectrum::new(50.0);
+    let light_to_world: Transform = Transform::default();
+    let _point_light: PointLight =
+        PointLight::new(&light_to_world, &MediumInterface::default(), &i);
+}
