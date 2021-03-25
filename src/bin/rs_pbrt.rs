0288185ad8c30c0456047685e63ed3f23d08c321
Jan Walter <jdb.walter@posteo.de>
2021-03-25 14:40:26 +0100

    The copyright message should include the year 2021.

--- src/bin/rs_pbrt.rs
+++ src/bin/rs_pbrt.rs
@@ -858,7 +858,7 @@ fn main() {
         "pbrt version {} ({}) [Detected {} cores]",
         VERSION, git_describe, num_cores
     );
-    println!("Copyright (c) 2016-2020 Jan Douglas Bert Walter.");
+    println!("Copyright (c) 2016-2021 Jan Douglas Bert Walter.");
     println!("Rust code based on C++ code by Matt Pharr, Greg Humphreys, and Wenzel Jakob.");
     let (mut api_state, mut bsdf_state) = pbrt_init(number_of_threads);
     parse_file(
