7388594cb7733487375a8874bba2d194d5df151d
Jan Walter <jdb.walter@posteo.de>
2021-09-12 17:24:59 +0200

    The Sphinx documentation for readthedocs.io specified wrong release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Jan Douglas Bert Walter'
 author = 'Jan Douglas Bert Walter'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.3'
+release = '0.9.3'
 
 
 # -- General configuration ---------------------------------------------------
@@ -52,4 +52,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+##html_static_path = ['_static']
--- docs/source/license.rst
+++ docs/source/license.rst
@@ -42,9 +42,9 @@ DEALINGS IN THE SOFTWARE.
 Apache License, Version 2.0
 ---------------------------
 
-                              Apache License
-                        Version 2.0, January 2004
-                     http://www.apache.org/licenses/
+Apache License
+Version 2.0, January 2004
+http://www.apache.org/licenses/
 
 TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
 
