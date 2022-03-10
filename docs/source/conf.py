f57451f2af5d9a410d514fe307f5532c08a480c5
Jan Walter <jdb.walter@posteo.de>
2022-03-10 16:09:56 +0100

    Prepare v0.9.6 release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Jan Douglas Bert Walter'
 author = 'Jan Douglas Bert Walter'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.5'
+release = '0.9.6'
 
 
 # -- General configuration ---------------------------------------------------
